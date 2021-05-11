8d0ffbac09ebd905ad77ab996183bd1e1c92d494
Aiven <aiven@timptner.de>
2021-05-11 20:07:27 +0200

    Set up new app

--- stura/settings/base.py
+++ stura/settings/base.py
@@ -11,6 +11,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # Application definition
 
 INSTALLED_APPS = [
+    'award',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
