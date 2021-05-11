2dd9042b2117936f22d7e2b15d00424b78733212
Aiven <aiven@timptner.de>
2021-05-11 22:00:14 +0200

    Add default representations

--- award/models.py
+++ award/models.py
@@ -11,6 +11,9 @@ class Lecturer(models.Model):
             models.UniqueConstraint(fields=['first_name', 'last_name'], name='unique fullname')
         ]
 
+    def __str__(self):
+        return self.full_name()
+
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
 
@@ -19,6 +22,9 @@ class Module(models.Model):
     name = models.CharField(max_length=250, unique=True)
     lecturers = models.ManyToManyField(Lecturer)
 
+    def __str__(self):
+        return self.name
+
 
 class Nomination(models.Model):
     lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
@@ -26,3 +32,6 @@ class Nomination(models.Model):
     reason = models.TextField()
     sub_email = models.EmailField(unique=True, validators=[EmailValidator(allowlist=['st.ovgu.de', 'ovgu.de'])])
     sub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sub_email
