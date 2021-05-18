bb47b45e6aa40f969f7f613df77d606a31ab6758
Aiven <aiven@timptner.de>
2021-05-18 20:42:06 +0200

    Add verification view

--- award/views.py
+++ award/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import ListView, TemplateView, FormView
 
 from .forms import SubmissionForm
-from .models import Lecturer
+from .models import Lecturer, Verification
 
 
 class LecturerListView(ListView):
@@ -30,6 +31,39 @@ class SubmissionSuccessView(TemplateView):
     template_name = 'award/submission_success.html'
 
 
-def verify_token(request):
-    # TODO add verification logic & replace template
-    return render(request, 'award/submission_success.html')
+def verify_token(request, token):
+    try:
+        verification = Verification.objects.get(pk=token)
+    except Verification.DoesNotExist:
+        feedback = {
+            'valid_token': False,
+            'title': "Token existiert nicht",
+            'message': "Der verwendete Link ist beschädigt oder veraltet. Bitte verwende nur "
+                       "den Link, welchen du per E-Mail von uns bekommen hast.",
+        }
+        return render(request, 'award/verification.html', {'feedback': feedback})
+
+    if timezone.now() > verification.expiration:
+        expiry = timezone.make_naive(verification.expiration).strftime('%d.%m.%y um %H:%M Uhr')
+        feedback = {
+            'valid_token': False,
+            'title': "Token ist abgelaufen",
+            'message': f"Der verwendete Link war nur bis zum {expiry} gültig.",
+        }
+        return render(request, 'award/verification.html', {'feedback': feedback})
+
+    nomination = verification.nomination
+    verification.delete()
+
+    nomination.verified = True
+    nomination.save()
+
+    lecturer = nomination.lecturer
+
+    feedback = {
+        'valid_token': True,
+        'title': "Token ist gültig",
+        'message': f"Dein Vorschlag für {lecturer.get_full_name()} wurde erfolgreich bestätigt."
+    }
+
+    return render(request, 'award/verification.html', {'feedback': feedback})
