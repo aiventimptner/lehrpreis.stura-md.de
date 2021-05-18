c47267747a0da8c8b8ccab4d41434b5556a3da8d
Aiven <aiven@timptner.de>
2021-05-18 22:22:50 +0200

    Allow email auth for development

--- stura/settings/development.py
+++ stura/settings/development.py
@@ -22,3 +22,7 @@ DATABASES = {
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
 
 EMAIL_PORT = os.getenv('EMAIL_PORT', 1025)
+
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
