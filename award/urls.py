2fc98cbd83b2e88928f5ec35a2c475c3604c7805
Aiven <aiven@timptner.de>
2021-05-19 21:25:48 +0200

    Use simpler namespace

--- award/urls.py
+++ award/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import LecturerListView, SubmissionFormView, SubmissionSuccessView, verify_token
+from . import views
 
 urlpatterns = [
-    path('', LecturerListView.as_view(), name='lecturer-list'),
-    path('submit/', SubmissionFormView.as_view(), name='submission-create'),
-    path('submit/success/', SubmissionSuccessView.as_view(), name='submission-success'),
-    path('verify/<slug:token>/', verify_token, name='verify-token'),
+    path('', views.LecturerListView.as_view(), name='lecturer-list'),
+    path('submit/', views.SubmissionFormView.as_view(), name='submission-create'),
+    path('submit/success/', views.SubmissionSuccessView.as_view(), name='submission-success'),
+    path('verify/<slug:token>/', views.verify_token, name='verify-token'),
 ]
